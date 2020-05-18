70a526cc977bde68e7853e34c7b530137aab9db3
jan.arbet <jan.arbet@ultimum.io>
2020-05-18 18:28:04 +0200

    Add args parser

--- VersionStatus.py
+++ VersionStatus.py
@@ -5,6 +5,7 @@ import requests
 import jinja2
 import re
 import operator
+import argparse
 
 OS_VER_URI = "https://releases.openstack.org/{}"
 DEB_VER_URI = "http://buster-{}.debian.net/debian/dists/" \
@@ -15,28 +16,58 @@ STATUS_UP_TO_DATE = "2"
 STATUS_MISSING = "3"
 
 
-def render_page(page_file_name, data):
-    output = jinja2.Environment(
-        loader=jinja2.FileSystemLoader('./templates/')) \
-        .get_template("index-template.j2") \
-        .render(packages_versions_data=data)
-    with open(page_file_name, 'w') as f:
-        f.write(output)
+def parse_args():
+    parser = argparse.ArgumentParser(description='Os-Version-Checker will '
+                                                 'check and compare self '
+                                                 'from upstream against '
+                                                 'debian.')
+    parser.add_argument('-f',
+                        nargs='?',
+                        help='file name for render (default: index.html)',
+                        metavar='file_name',
+                        type=str,
+                        default="index.html",
+                        dest="file_name")
+    parser.add_argument('-o',
+                        nargs='?',
+                        help='option for render (default: page)',
+                        metavar='"page"/"text"',
+                        type=str,
+                        default="page",
+                        dest="option")
+    return parser.parse_args()
+
+
+class Renderer:
+    def __init__(self, data, file_name):
+        self.data = data
+        self.file_name = file_name
+
+    def render(self, option):
+        if "text" in option:
+            output = yaml.dump(self.data)
+        if "page" in option:
+            output = jinja2.Environment(
+                loader=jinja2.FileSystemLoader('./templates/')) \
+                .get_template("index-template.j2") \
+                .render(packages_versions_data=self.data)
+        with open(self.file_name, 'w') as f:
+            f.write(output)
 
 
 class VersionsData:
-    def __init__(self, release):
+    def __init__(self, _release):
         self.url_deb_content = requests \
-            .get(DEB_VER_URI.format(release, release)).content.decode()
+            .get(DEB_VER_URI.format(_release, _release)).content.decode()
         self.url_os_content = requests \
-            .get(OS_VER_URI.format(release)).content.decode()
+            .get(OS_VER_URI.format(_release)).content.decode()
         self.deb_data = self.get_debian_versions()
         self.os_data = self.get_upstream_versions()
         self.versions_data = self.compare_versions()
 
     def get_upstream_versions(self):
         # get all links, which ends .tar.gz from HTML
-        links = re.findall("https://.*\.tar\.gz", self.url_os_content)
+        links = re.findall("https://.*.tar.gz", self.url_os_content)
         results = dict()
         for pkg_link in links:
             # get name and package informations from link
@@ -50,7 +81,7 @@ class VersionsData:
             # and check for higher version
             if pkg_name2 not in results:
                 pkg_info = dict(version=pkg_ver, href=pkg_link)
-                results.update([(pkg_name2, pkg_info)])
+                results[pkg_name2] = pkg_info
             else:
                 # if current versions < new version, than update it
                 if version.parse(results.get(pkg_name2).get('version')) \
@@ -72,11 +103,11 @@ class VersionsData:
                                 str(pkg_info.get('Version'))).group(2)
             pkg_link = pkg_info.get('Vcs-Browser')
             pkg_info = dict(version=pkg_ver, href=pkg_link)
-            results.update([(pkg_name, pkg_info)])
+            results[pkg_name] = pkg_info
         return results
 
     def get_deb_pair(self, os_pkg_name):
-        def sanitize_os_pkg_name(os_pkg_name, str_to_replace):
+        def sanitize_os_pkg_name(_os_pkg_name, str_to_replace):
             default_replace = os_pkg_name.replace("_", "-")
             cases = {
                 "-": default_replace,
@@ -86,11 +117,12 @@ class VersionsData:
             }
             return cases.get(str_to_replace)
 
-        def is_in_deb_data(os_pkg_name, replacement):
+        def is_in_deb_data(_os_pkg_name, _replacement):
             if sanitize_os_pkg_name(os_pkg_name, replacement) in self.deb_data:
                 return sanitize_os_pkg_name(os_pkg_name, replacement)
             return False
 
+        # try find modified debian package name in debian packages
         replacements = ["-", "python-", "puppet-", "openstack-"]
         for replacement in replacements:
             if is_in_deb_data(os_pkg_name, replacement):
@@ -128,7 +160,7 @@ class VersionsData:
                                  upstream_package_version=
                                  os_pkg_ver,
                                  status=STATUS_MISSING)
-            result.update([(os_pkg_name, pkg_infos)])
+            result[os_pkg_name] = pkg_infos
 
         print("UPSTREAM_PACKAGES: {}    DEBIAN_PACKAGES: {} PAIRED: {}"
               .format(len(self.os_data), len(self.deb_data) + paired, paired))
@@ -141,9 +173,12 @@ class VersionsData:
 
 if __name__ == '__main__':
 
+    args = parse_args()
+
     ver_data = dict()
     for release in RELEASES:
         release_ver_dat = VersionsData(release)
-        ver_data.update([(release, release_ver_dat.get_versions_data())])
+        ver_data[release] = release_ver_dat.get_versions_data()
 
-    render_page("index.html", ver_data)
+    renderer = Renderer(ver_data, args.file_name)
+    renderer.render(args.option)
