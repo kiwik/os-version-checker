faedda47e7a0a72b57050dd2211e502d0537bd9a
jan.arbet <jan.arbet@ultimum.io>
2020-05-18 15:42:51 +0200

    fixovanie bugov

--- VersionStatus.py
+++ VersionStatus.py
@@ -7,119 +7,143 @@ import re
 import operator
 
 OS_VER_URI = "https://releases.openstack.org/{}"
-DEB_VER_URI = "http://buster-{}.debian.net/debian/dists/buster-{}-backports/main/source/Sources"
+DEB_VER_URI = "http://buster-{}.debian.net/debian/dists/" \
+              "buster-{}-backports/main/source/Sources"
 RELEASES = ["stein", "train", "ussuri"]
-
-DEFAULT = "None"
-
-VERSION = "Version"
-STATUS = "Status"
-PACKAGE = "Package"
-VCS_BROWSER = "Vcs-Browser"
-
 STATUS_OUT_OF_DATE = "1"
-STATUS2_UP_TO_DATE = "2"
+STATUS_UP_TO_DATE = "2"
 STATUS_MISSING = "3"
 
 
-class GeneratorHTMLVersionStatus:
-
-    @staticmethod
-    def render_page(page_file_name, data):
-        output = jinja2.Environment(loader=jinja2.FileSystemLoader('./templates/')) \
-            .get_template("index-template.j2") \
-            .render(packages_versions_data=data)
-        print
-        output
-        with open(page_file_name, 'w') as f:
-            f.write(output)
+def render_page(page_file_name, data):
+    output = jinja2.Environment(
+        loader=jinja2.FileSystemLoader('./templates/')) \
+        .get_template("index-template.j2") \
+        .render(packages_versions_data=data)
+    with open(page_file_name, 'w') as f:
+        f.write(output)
 
 
-class HTMLVersionsParser:
+class VersionsData:
+    def __init__(self, release):
+        self.url_deb_content = requests \
+            .get(DEB_VER_URI.format(release, release)).content.decode()
+        self.url_os_content = requests \
+            .get(OS_VER_URI.format(release)).content.decode()
+        self.deb_data = self.get_debian_versions()
+        self.os_data = self.get_upstream_versions()
+        self.versions_data = self.compare_versions()
 
-    @staticmethod
-    def get_upstream_versions(url):
+    def get_upstream_versions(self):
         # get all links, which ends .tar.gz from HTML
-        links = re.findall('https://.*\.tar\.gz', requests.get(url).content.decode())
+        links = re.findall("https://.*\.tar\.gz", self.url_os_content)
         results = dict()
         for pkg_link in links:
             # get name and package informations from link
             tmp = pkg_link.split("/")
             pkg_name = tmp[3]
             pkg_full_name = tmp[4]
-            pkg_ver = pkg_full_name[pkg_full_name.rfind('-') + 1:pkg_full_name.rfind('.tar')]
-            # check if package with version are in results, and check for higher version
-            if pkg_name not in results:
-                pkg_info = dict(Version=pkg_ver, Href=pkg_link)
-                results.update([(pkg_name, pkg_info)])
+            pkg_name2 = pkg_full_name[0:pkg_full_name.rfind('-')]
+            pkg_ver = pkg_full_name[pkg_full_name.rfind('-') + 1
+                                    :pkg_full_name.rfind('.tar')]
+            # check if package with version are in results,
+            # and check for higher version
+            if pkg_name2 not in results:
+                pkg_info = dict(version=pkg_ver, href=pkg_link)
+                results.update([(pkg_name2, pkg_info)])
             else:
                 # if current versions < new version, than update it
-                if version.parse(results.get(pkg_name).get(VERSION)) < version.parse(pkg_ver):
-                    results.get(pkg_name).update(Version=pkg_ver)
+                if version.parse(results.get(pkg_name2).get('version')) \
+                        < version.parse(pkg_ver):
+                    results.get(pkg_name2).update(version=pkg_ver)
         return results
 
-    @staticmethod
-    def get_debian_versions(url):
+    def get_debian_versions(self):
         # get all yaml package info from debian HTML
-        pkg_info_yamls = [x.replace(": @", ": ") for x in requests.get(url).content.decode().split('\n\n') if x != '']
+        pkg_info_yamls = [x.replace(": @", ": ")
+                          for x in
+                          self.url_deb_content.split('\n\n')
+                          if x != '']
         results = dict()
         for pkg_info_yaml in pkg_info_yamls:
             pkg_info = yaml.safe_load(pkg_info_yaml)
-            pkg_name = pkg_info.get(PACKAGE)
-            pkg_ver = re.search('([\d]+:)?([^-]+)([-~+].+)?', str(pkg_info.get(VERSION))).group(2)
-            pkg_link = pkg_info.get(VCS_BROWSER)
-            pkg_info = dict(Version=pkg_ver, Href=pkg_link)
+            pkg_name = pkg_info.get('Package')
+            pkg_ver = re.search('([0-9]+:)?([^-]+)([-~+].+)?',
+                                str(pkg_info.get('Version'))).group(2)
+            pkg_link = pkg_info.get('Vcs-Browser')
+            pkg_info = dict(version=pkg_ver, href=pkg_link)
             results.update([(pkg_name, pkg_info)])
         return results
 
+    def get_deb_pair(self, os_pkg_name):
+        def sanitize_os_pkg_name(os_pkg_name, str_to_replace):
+            default_replace = os_pkg_name.replace("_", "-")
+            cases = {
+                "-": default_replace,
+                "python-": "python-{}".format(default_replace),
+                "openstack-": default_replace.replace("openstack-", ""),
+                "puppet-": default_replace.replace("puppet-", "puppet-module-")
+            }
+            return cases.get(str_to_replace)
 
-class VersionStatus:
+        def is_in_deb_data(os_pkg_name, replacement):
+            if sanitize_os_pkg_name(os_pkg_name, replacement) in self.deb_data:
+                return sanitize_os_pkg_name(os_pkg_name, replacement)
+            return False
 
-    @staticmethod
-    def deb_pair(deb_data, os_pkg_name):
-        # new_upstream_package_name = os_pkg_name.replace("_", "-")
-        if os_pkg_name.replace("_", "-") in deb_data:
-            return os_pkg_name.replace("_", "-")
-        elif ("python-" + os_pkg_name.replace("_", "-")) in deb_data:
-            return "python-" + os_pkg_name.replace("_", "-")
-        elif re.match('^puppet-', os_pkg_name.replace("_", "-")):
-            if os_pkg_name.replace("_", "-").replace("puppet-", "puppet-module-") in deb_data:
-                return os_pkg_name.replace("_", "-").replace("puppet-", "puppet-module-")
+        replacements = ["-", "python-", "puppet-", "openstack-"]
+        for replacement in replacements:
+            if is_in_deb_data(os_pkg_name, replacement):
+                return sanitize_os_pkg_name(os_pkg_name, replacement)
 
-    @staticmethod
-    def compare_versions(os_data, deb_data):
+    def compare_versions(self):
+        def set_status(os_ver, deb_ver):
+            if "+" in deb_ver:
+                deb_ver = deb_ver.split('+')[0]
+            if "~" in deb_ver:
+                deb_ver = deb_ver.split('~')[0]
+            if version.parse(os_ver) == version.parse(deb_ver):
+                return STATUS_UP_TO_DATE
+            else:
+                return STATUS_OUT_OF_DATE
+
         result = dict()
         paired = 0
-        for os_pkg_name, os_pkg_info in os_data.items():
-            deb_pkg_name = VersionStatus.deb_pair(deb_data, os_pkg_name)
-            # if debian package a openstack package have pair, then check version
+        for os_pkg_name, os_pkg_info in self.os_data.items():
+            deb_pkg_name = self.get_deb_pair(os_pkg_name)
+            os_pkg_ver = os_pkg_info.get('version')
+            # if debian and openstack package have pair
             if deb_pkg_name is not None:
-                pkg_infos = dict(debian_package_version=deb_data.get(deb_pkg_name).get(VERSION),
-                                 upstream_package_version=os_pkg_info.get(VERSION),
-                                 Status=DEFAULT)
-                if version.parse(os_pkg_info.get(VERSION)) == version.parse(deb_data.get(deb_pkg_name).get(VERSION)):
-                    pkg_infos.update(Status=STATUS2_UP_TO_DATE)
-                else:
-                    pkg_infos.update(Status=STATUS_OUT_OF_DATE)
+                deb_pkg_info = self.deb_data.get(deb_pkg_name)
+                deb_pkg_ver = deb_pkg_info.get('version')
+                pkg_infos = dict(debian_package_version
+                                 =deb_pkg_ver,
+                                 upstream_package_version
+                                 =os_pkg_ver,
+                                 status=set_status(os_pkg_ver, deb_pkg_ver))
                 paired += 1
-                del deb_data[deb_pkg_name]
+                del self.deb_data[deb_pkg_name]
             else:
-                pkg_infos = dict(debian_package_version=DEFAULT,
-                                 upstream_package_version=os_pkg_info.get(VERSION),
-                                 Status=STATUS_MISSING)
+                pkg_infos = dict(debian_package_version=None,
+                                 upstream_package_version=
+                                 os_pkg_ver,
+                                 status=STATUS_MISSING)
             result.update([(os_pkg_name, pkg_infos)])
 
         print("UPSTREAM_PACKAGES: {}    DEBIAN_PACKAGES: {} PAIRED: {}"
-              .format(len(os_data), len(deb_data) + paired, paired))
-        return OrderedDict(sorted(result.items(), key=lambda x: operator.getitem(x[1], STATUS)))
+              .format(len(self.os_data), len(self.deb_data) + paired, paired))
+        return OrderedDict(sorted(
+            result.items(), key=lambda x: operator.getitem(x[1], 'status')))
+
+    def get_versions_data(self):
+        return self.versions_data
 
 
 if __name__ == '__main__':
 
-    pkg_ver_data = dict()
+    ver_data = dict()
     for release in RELEASES:
-        os_ver_data = HTMLVersionsParser.get_upstream_versions(OS_VER_URI.format(release))
-        deb_ver_data = HTMLVersionsParser.get_debian_versions(DEB_VER_URI.format(release, release))
-        pkg_ver_data.update([(release, VersionStatus.compare_versions(os_ver_data, deb_ver_data))])
+        release_ver_dat = VersionsData(release)
+        ver_data.update([(release, release_ver_dat.get_versions_data())])
 
-    GeneratorHTMLVersionStatus.render_page("index.html", pkg_ver_data)
+    render_page("index.html", ver_data)
