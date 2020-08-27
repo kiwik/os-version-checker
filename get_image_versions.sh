7adef76b79a5ce9e5de666e39c4df8d5e519d8be
Jan Arbet <jan.arbet@ultimum.io>
2020-08-27 13:39:49 +0200

    Add sleep to script

--- get_image_versions.sh
+++ get_image_versions.sh
@@ -4,3 +4,4 @@ if [ ! -d "./images_versions/$2" ]; then
     mkdir -p "./images_versions/$2"
 fi
 dpkg-query -W --showformat '${Status} ${Package} ${Version}\n' $(dpkg -l | tail -n +6 | tr -s ' ' | cut -d ' ' -f2) | grep ^install | cut -d ' ' -f4,5 > ./images_versions/"$2"/"$1".txt
+sleep 20m
