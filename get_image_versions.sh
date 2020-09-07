8b8e64526c724356606664db5976733256c40987
Jan Arbet <jan.arbet@ultimum.io>
2020-09-07 11:12:32 +0200

    Add another tag and images to testing template and test new method of get version data

--- get_image_versions.sh
+++ get_image_versions.sh
@@ -3,4 +3,6 @@
 if [ ! -d "./images_versions/$2" ]; then
     mkdir -p "./images_versions/$2"
 fi
-dpkg-query -W --showformat '${Status} ${Package} ${Version}\n' $(dpkg -l | tail -n +6 | tr -s ' ' | cut -d ' ' -f2) | grep ^install | cut -d ' ' -f4,5 > ./images_versions/"$2"/"$1".txt
+#dpkg-query -W --showformat '${Status} ${Package} ${Version}\n' $(dpkg -l | tail -n +6 | tr -s ' ' | cut -d ' ' -f2) | grep ^install | cut -d ' ' -f4,5 > ./images_versions/"$2"/"$1".txt
+apt-get update
+apt-get --just-print upgrade 2>&1 | perl -ne 'if (/Inst\s([\w,\-,\d,\.,~,:,\+]+)\s\[([\w,\-,\d,\.,~,:,\+]+)\]\s\(([\w,\-,\d,\.,~,:,\+]+)\)? /i) {print "$1 $2 $3\n"}' && dpkg-query -W --showformat '${Status} ${Package} ${Version}\n' $(dpkg -l | tail -n +6 | tr -s ' ' | cut -d ' ' -f2) | grep ^install | cut -d ' ' -f4,5 > ./images_versions/"$2"/"$1".txt
