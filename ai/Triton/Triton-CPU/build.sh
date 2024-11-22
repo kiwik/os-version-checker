7b2186c5c3efccfa541fb949f19ad330a36e95f3
RuiChen <chenrui.momo@gmail.com>
2024-11-22 10:00:42 +0800

    Add gcc for openEuler customizer Triton LLVM

--- ai/Triton/Triton-CPU/build.sh
+++ ai/Triton/Triton-CPU/build.sh
@@ -8,9 +8,11 @@ source ~/.bashrc
 conda create -y -n triton-cpu python=3.12
 conda activate triton-cpu
 
-# Install deps
+# Install deps for Triton
 conda install -y ninja cmake wheel pybind11
 conda install -y -c conda-forge libstdcxx-ng=12
+# Install deps for LLVM
+conda install -y -c conda-forge gcc=14 gxx=14
 
 # Config build env
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
@@ -21,14 +23,17 @@ cd triton-cpu
 
 ## Customizer LLVM for openEuler aarch64
 llvm_version=$(cat cmake/llvm-hash.txt)
-git clone --recursive https://github.com/llvm/llvm-project.git
-cd llvm-project
-git checkout -b pin-version $llvm_version
+mkdir llvm-project; cd llvm-project
+git init
+git remote add origin https://github.com/llvm/llvm-project.git
+git fetch --depth 1 origin $llvm_version
+git checkout -b pin-version FETCH_HEAD
 
 ## Build LLVM for Triton
-mkdir build
-cd build
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON ../llvm -DLLVM_ENABLE_PROJECTS="mlir;llvm" -DLLVM_TARGETS_TO_BUILD="host;NVPTX;AMDGPU"
+mkdir build; cd build
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON ../llvm \
+  -DLLVM_ENABLE_PROJECTS="mlir;llvm" \
+  -DLLVM_TARGETS_TO_BUILD="host;NVPTX;AMDGPU"
 ninja
 cd ../..
 
