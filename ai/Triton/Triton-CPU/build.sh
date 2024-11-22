d01270c7df9e67b40e6b176f50a27cf28be24c00
RuiChen <chenrui.momo@gmail.com>
2024-11-22 09:00:30 +0800

    Build customizer LLVM for openEuler aarch64 Triton

--- ai/Triton/Triton-CPU/build.sh
+++ ai/Triton/Triton-CPU/build.sh
@@ -6,18 +6,33 @@ conda init bash
 source ~/.bashrc
 
 conda create -y -n triton-cpu python=3.12
-git clone --recursive https://github.com/triton-lang/triton-cpu.git;
-cd triton-cpu
-
 conda activate triton-cpu
 
-# deps
+# Install deps
 conda install -y ninja cmake wheel pybind11
 conda install -y -c conda-forge libstdcxx-ng=12
 
-# build
+# Config build env
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 echo $LD_LIBRARY_PATH
+
+git clone --recursive https://github.com/triton-lang/triton-cpu.git
+cd triton-cpu
+
+## Customizer LLVM for openEuler aarch64
+llvm_version=$(cat cmake/llvm-hash.txt)
+git clone --recursive https://github.com/llvm/llvm-project.git
+cd llvm-project
+git checkout -b pin-version $llvm_version
+
+## Build LLVM for Triton
+mkdir build
+cd build
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON ../llvm -DLLVM_ENABLE_PROJECTS="mlir;llvm" -DLLVM_TARGETS_TO_BUILD="host;NVPTX;AMDGPU"
+ninja
+cd ../..
+
+## Build Triton
 pip install -e python
 
 conda deactivate
