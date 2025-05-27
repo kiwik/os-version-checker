919781b76c7c5b04d15fc77c0de2bf6ac6b0ee9d
RuiChen <chenrui.momo@gmail.com>
2025-05-27 10:08:17 +0800

    Update base ai agent

--- ai/talk-with-all-llms/base-ai-agent.py
+++ ai/talk-with-all-llms/base-ai-agent.py
@@ -8,18 +8,22 @@ from openai import OpenAI, DefaultHttpxClient
 with open('config.toml', 'rb') as f:
     config_data = tomllib.load(f)
 
-proxy_httpx_client = DefaultHttpxClient(
-    proxy=config_data['proxy']['base_url'],
-    transport=httpx.HTTPTransport(local_address='0.0.0.0'),
-    verify=config_data['proxy']['verify'])
+_headers = {'Authorization':
+                'Bearer {}'.format(config_data['free-api-qwen']['api_key'])}
+_model_name = config_data['free-api-qwen']['model_name']
 
-client = OpenAI(api_key=config_data['llm']['api_key'],
-                base_url=config_data['llm']['base_url'],
+proxy_httpx_client = DefaultHttpxClient(
+    # proxy=config_data['proxy']['base_url'],
+    # transport=httpx.HTTPTransport(local_address='0.0.0.0'),
+    # verify=config_data['proxy']['verify'],
+    headers=_headers)
+
+client = OpenAI(api_key=config_data['free-api-qwen']['api_key'],
+                base_url=config_data['free-api-qwen']['base_url'],
                 http_client=proxy_httpx_client)
 
 conversation = [{'role': 'user',
-                 'content': '深度思考模式，每次输出以<think>\n开头。'
-                            '输出不要使用markdown格式。'
+                 'content': '输出不要使用markdown格式。'
                             '简洁回答，有启发性。'
                  }]
 while True:
@@ -32,7 +36,7 @@ while True:
                          'content': content})
 
     response = client.chat.completions.create(
-        model='deepseek/deepseek-r1:free',
+        model=_model_name,
         temperature=0.6,
         messages=conversation,
         stream=True
