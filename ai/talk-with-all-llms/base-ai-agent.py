122a467d11f65b740004e05ea92a099674906ac6
ChenRui <chenrui.momo@gmail.com>
2025-06-09 15:53:06 +0800

    Update common config

--- ai/talk-with-all-llms/base-ai-agent.py
+++ ai/talk-with-all-llms/base-ai-agent.py
@@ -1,16 +1,16 @@
 # Please install OpenAI SDK first: `pip3 install openai`
 
-import httpx
 import tomllib
-from openai import OpenAI, DefaultHttpxClient
-
+from openai import DefaultHttpxClient, OpenAI
 
 with open('config.toml', 'rb') as f:
     config_data = tomllib.load(f)
 
+_provider_config = config_data['free-api-qwen']
+
 _headers = {'Authorization':
-                'Bearer {}'.format(config_data['free-api-qwen']['api_key'])}
-_model_name = config_data['free-api-qwen']['model_name']
+                'Bearer {}'.format(_provider_config['api_key'])}
+_model_name = _provider_config['model_name']
 
 proxy_httpx_client = DefaultHttpxClient(
     # proxy=config_data['proxy']['base_url'],
@@ -18,13 +18,13 @@ proxy_httpx_client = DefaultHttpxClient(
     # verify=config_data['proxy']['verify'],
     headers=_headers)
 
-client = OpenAI(api_key=config_data['free-api-qwen']['api_key'],
-                base_url=config_data['free-api-qwen']['base_url'],
+client = OpenAI(api_key=_provider_config['api_key'],
+                base_url=_provider_config['base_url'],
                 http_client=proxy_httpx_client)
 
 conversation = [{'role': 'user',
-                 'content': '输出不要使用markdown格式。'
-                            '简洁回答，有启发性。'
+                 'content':
+                     '简洁回答，有启发性。'
                  }]
 while True:
     content = input('Input: ')
@@ -37,7 +37,7 @@ while True:
 
     response = client.chat.completions.create(
         model=_model_name,
-        temperature=0.6,
+        temperature=0.0,
         messages=conversation,
         stream=True
     )
